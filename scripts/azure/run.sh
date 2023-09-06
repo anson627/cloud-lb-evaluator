ffa0f74ffea15aa642cd7dcdcd194144aa85006a
alyssavu <alyssavu@microsoft.com>
2023-09-06 16:05:32 +0000

    customize iterations

--- scripts/azure/run.sh
+++ scripts/azure/run.sh
@@ -3,6 +3,7 @@
 tag=$1
 side=$2
 public_ip=${3:-''}
+iteration=${4:-1000000}
 
 cd /home/adminuser
 curl -L -s -o ${side}.tar.gz https://github.com/anson627/cloud-lb-evaluator/releases/download/$tag/${side}.tar.gz
@@ -11,7 +12,7 @@ rm ${side}.tar.gz
 
 cd ${side}_build
 if [ ! -z "$side" ]; then
-  ./${side} $public_ip &> logs.txt &
+  ./${side} $public_ip $iteration &> logs.txt &
 else
   ./${side} &> logs.txt &
 fi
