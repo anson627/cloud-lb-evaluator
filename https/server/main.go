42a10441ccb8319f98f4b25ea81532581cc9c1d0
alyssavu <alyssavu@microsoft.com>
2023-09-07 22:19:09 +0000

    set timeout to 10s

--- https/server/main.go
+++ https/server/main.go
@@ -77,7 +77,7 @@ func main() {
 	httpsServer := &http.Server{
 		Addr:              ":4443",
 		TLSConfig:         tlsConfig,
-		ReadHeaderTimeout: 32 * time.Second, // https://github.com/kubernetes/kubernetes/blob/aa8cb97f65fe1d24e96eda129337d86109615570/staging/src/k8s.io/apiserver/pkg/server/secure_serving.go#L172
+		ReadHeaderTimeout: 10 * time.Second, // https://github.com/kubernetes/kubernetes/blob/aa8cb97f65fe1d24e96eda129337d86109615570/staging/src/k8s.io/apiserver/pkg/server/secure_serving.go#L172
 	}
 	go func() {
 		defer wg.Done()
